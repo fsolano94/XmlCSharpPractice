eb2fe5884cfe81ac2bfae36a5452edb175a1f063
Franky Solano <fsolano@nevada.unr.edu>
2018-05-19 10:39:42 -0700

    Added more methods using Linq to Xml

--- Xml/Program.cs
+++ Xml/Program.cs
@@ -18,11 +18,39 @@ namespace Xml
         public string Year { get; set; } = DateTime.Now.Year.ToString();
     }
 
+    public class Book
+    {
+        public string Author { get; set; }
+        public string DatePublished { get; set; }
+        public string Genre { get; set; }
+        public string Title { get; set; }
+        public int Rating { get; set; }
+
+
+        public static List<Book> GetAllBooks()
+        {
+            return new List<Book>()
+            {
+                new Book(){Author  = "Robert C. Martin", DatePublished = "Aug. 1999", Genre = "Arch.", Title = "CleanCode", Rating = 10},
+                new Book(){Author = "Martin Fowler", DatePublished = "Sept. 2000", Genre = "Arch.", Title = "Refactoring", Rating = 9 },
+                new Book(){Author = "Gregor Hohpe", DatePublished = "Oct. 2010", Genre = "Design Patterns", Title = "Enterprise Software", Rating = 8},
+                new Book(){Author = "Eric Evans", DatePublished = "Jan. 2017", Genre = "DDD", Title = "Tackling DDD", Rating = 7}
+            };
+        }
+    }
+
 
     class Program
     {
 
         static void Main(string[] args)
+        {
+            Test_QueryXmlFile();
+           
+            Console.ReadLine();
+        }
+
+        public static void Test_1()
         {
             string fileName = "abc.xml";
 
@@ -32,7 +60,7 @@ namespace Xml
             {
                 dates.Add(new Date() { Id = i + 1 });
             }
-            
+
             var dateAsXmlString = SerializeDateToXmlString(dates);
 
             Console.WriteLine("====================================");
@@ -52,7 +80,6 @@ namespace Xml
 
             ModifyDateElementWithSpecifiedId(fileName, 4, "Day", "SomeNewTestDay");
 
-            Console.ReadLine();
         }
 
         public static string SerializeDateToXmlString(List<Date> dates)
@@ -98,5 +125,102 @@ namespace Xml
 
             xmlDocument.Save(filePathAndNameOfXmlDocument);
         }
+
+        public static void CreateXmlDocument(string nameOfRootElement, string nameOfChildElement, string comment, int childElementId, string fullPathToXmlFileThatWillBeCreated)
+        {
+
+            Console.WriteLine($"Creating file {fullPathToXmlFileThatWillBeCreated} ...");
+
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment(comment),
+                new XElement(nameOfRootElement,
+                new XElement(nameOfChildElement,
+                    new XAttribute("id", childElementId)
+                )));
+
+            xmlDocument.Save(fullPathToXmlFileThatWillBeCreated);
+
+            Console.WriteLine("File created successfully.");
+        }
+
+        public static void Test_CreateXmlDocument()
+        {
+            string fileName = string.Empty;
+            string rootElementName = string.Empty;
+            string childElementName = string.Empty;
+            string comment = string.Empty;
+            int idOfChildElement = 0;
+            
+            Console.Write("Enter root element name: ");
+
+            rootElementName = Console.ReadLine();
+
+            Console.Write("Enter child element name: ");
+
+            childElementName = Console.ReadLine();
+
+            Console.Write("Enter id of child element: ");
+
+            idOfChildElement = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter comment: ");
+
+            comment = Console.ReadLine();
+
+            Console.Write("Enter full path and name of file that will be created: ");
+
+            fileName = Console.ReadLine();
+
+            CreateXmlDocument(rootElementName, childElementName, comment, idOfChildElement, fileName);
+
+        }
+
+        public static void Test_CreateXmlDocumentFromListOfElements()
+        {
+            string fileName = string.Empty;
+
+            Console.Write("Enter the file name and location where the list of books will be stored: ");
+
+            fileName = Console.ReadLine();
+
+            Console.WriteLine($"Now creating file {fileName}.");
+
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("This list of books was made up for learning how to make this file :)"),
+                new XElement("Books",
+                        from book in Book.GetAllBooks() select
+                        new XElement("Book",
+                            new XElement("Author", book.Author),
+                            new XElement("DatePublished", book.DatePublished),
+                            new XElement("Genre", book.Genre),
+                            new XElement("Title", book.Title),
+                            new XElement("Rating", book.Rating)
+                            )
+                ));
+
+            xmlDocument.Save(fileName);
+
+            Console.WriteLine($"Successfully created file {fileName}.");
+        }
+
+        public static void Test_QueryXmlFile()
+        {
+            var bookTitles = (from book in XDocument.Load(@"C:\listOfBooks.xml")
+                             .Descendants("Book")
+                             where (int)book.Element("Rating") > 9
+                             orderby (int)book.Element("Rating") descending
+                             select book.Element("Title").Value).ToList();
+
+
+            for(int index = 1; index <= bookTitles.Count(); index++)
+            {
+                Console.WriteLine($"Title of book {index}: {bookTitles[index-1]}.");
+            }
+
+            
+        }
+
     }
 }
